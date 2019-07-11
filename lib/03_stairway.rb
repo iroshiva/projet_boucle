978e1e580b351a19d2e08e65b10bd03d61f8bc3c
iroshiva <jean.weil@protonmail.com>
2019-07-11 20:10:50 +0200

    fin exo stairway

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,5 +1,10 @@
 
-# class Game
+class Game
+
+	def initialize # method appliquée dès que nouvelle instance créée
+		@pos_mar = 0
+		@comp_tour = 0
+	end
 
 	def lance_de
 		# à chaque tour ===> lancé de dé
@@ -12,100 +17,83 @@
 
 		# initialiser le lancé de dé
 		# comp_tour
-		comp_tour += 1
+		@comp_tour += 1
 
-		$chifr = rand (1..6)
-		puts $chifr
-
-		
+		@chifr = rand(1..6)
 
 	end
 
-# puts lance_de
-
+	# puts lance_de
 	def verif_de
 		# - si 5 ou 6 : +1 (monter) et dire sur quelle marche est le joueur
-		if $chifr == 5 or $chifr == 6 # if @$chifr.to_s.match(/[56]/)
+		puts @chifr
+		if @chifr == 5 or @chifr == 6 # if @$chifr.to_s.match(/[56]/)
 			mont
-		elsif $chifr == 1
+		elsif @chifr == 1
 			desc
 		else 
 			rest
-
 		end
-			
+
 
 	end
 
 
 	def mont
-	# - si 5 ou 6 : +1 (monter) et dire sur quelle marche est le joueur
-		
-			$pos_mar += 1
-			puts "Vous êtes monté d'une marche!"
-			puts "Vous êtes maintenant à la marche #{pos_mar}"
+		# - si 5 ou 6 : +1 (monter) et dire sur quelle marche est le joueur
+
+		@pos_mar += 1
+		puts "Tour numéro #{@comp_tour}. Vous êtes monté d'une marche!"
+		puts "Vous êtes maintenant à la marche #{@pos_mar}"
 	end
 
-# # puts mont
+	# # puts mont
 
 	def desc
-	# - si 1 : -1 (descendre) et dire sur quelle marche est le joueur
-		if $pos_mar == 0
-			puts "Vous ne pouvez pas descendre plus bas!" 
-			puts "Vous êtes maintenant à la marche #{pos_mar}"
+		# - si 1 : -1 (descendre) et dire sur quelle marche est le joueur
+		if @pos_mar == 0
+			puts "Tour numéro #{@comp_tour}. Vous ne pouvez pas descendre plus bas!" 
+			puts "Vous êtes maintenant à la marche #{@pos_mar}"
 		else 
-			$pos_mar -= 1
+			@pos_mar -= 1
 			puts "Vous êtes descendu d'un marche!"
-			puts "Vous êtes maintenant à la marche #{pos_mar}"
+			puts "Vous êtes maintenant à la marche #{@pos_mar}"
 		end
 	end
 
 	def rest
-	# - si 2, 3 ou 4 : 0 (reste) et dire sur quelle marche est le joueur
-		puts "Vous restez perchés sur votre marche!"
-		puts "Vous êtes maintenant à la marche #{pos_mar}"
+		# - si 2, 3 ou 4 : 0 (reste) et dire sur quelle marche est le joueur
+		puts "Tour numéro #{@comp_tour}. Vous restez perchés sur votre marche!"
+		puts "Vous êtes maintenant à la marche #{@pos_mar}"
 	end
 
 
-	def initializing
-		$pos_mar = 0
-		$comp_tour = 0
-	end
+	
 
 	def verif_gagne
-		if $pos_mar == 10
+		if @pos_mar == 10
 			puts "Félicitation!!!!! Tu es arrivé à la dernière marche!!! "	# quand 10eme marche atteint : GAGNE !!!!!!
-			else
-				lance_de
+
 		end
 	end
 
-# # puts comp_ma
-
-	# def verif_gagne
-	# 	if $pos_mar == 10
-	# 		puts "Félicitation!!!!! Tu es arrivé à la dernière marche!!! "	# quand 10eme marche atteint : GAGNE !!!!!!
-	# 		else
-	# 			lance_de
-	# 	end
-	# end
+	def perform
+		while @pos_mar < 10
+			lance_de
+			verif_de
+			verif_gagne
+		end
 
 
-def perform
-	while $pos_mar < 10
-		pos_mar
-		lance_de
-		verif_de
-		verif_gagne
+		# additionne les autres method
 	end
-	
-	
-	# additionne les autres method
+
+
+
 end
 
-perform
-
-# end
+jeu = Game.new
+jeu.perform
 
 # var = Game.new
 # var.lance_de
