21842ecb1f367f8e97b6ba0dbc78ca4612725ecf
iroshiva <jean.weil@protonmail.com>
2019-07-11 15:44:18 +0200

    ajout exo 00

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,34 +1,20 @@
 
-
-def ask_first_name 
-	# definition de la methode ask_first_name chargée de demander le prenom de l'utilisateur
-	
-	puts "Quel est votre prénom?"
-	# demande le prenom de l'utilisateur
-	
+def ask_first_name
+  puts  "Quel est ton prénom ? "
 	print "> "
-	# l'utilisateur tape son prenom 
-	
 	first_name = gets.chomp
-	# variable first_name donné au prenom de l'utilisateur
-	
-	return first_name 
-	# retourne la variable first_name
+  	return first_name
 end
 
-
-def say_hello(first_name)
-	# definition de la methode say_hello chargé de dire bonjour à l'utilisateur avec une variable first_name 
-
-  	puts "Bonjour, #{first_name}!"
+def say_hello(prenom)
+  "Bonjour, #{prenom}!"
 end
 
 def perform
-	first_name = ask_first_name
-	say_hello(first_name)
-
-	# ou que say_hello(ask_first_name) = contraction des deux méthodes
+  prenom = ask_first_name
+  say_hello(prenom)
+  
 end
 
-# say_hello('jean')
-perform 
+
+puts perform
