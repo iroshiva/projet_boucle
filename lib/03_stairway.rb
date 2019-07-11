2d80ac7762298698f436bf8205b66b35f0c7c247
iroshiva <jean.weil@protonmail.com>
2019-07-11 17:24:38 +0200

    creation debut de methode

---
+++ lib/03_stairway.rb
@@ -0,0 +1,85 @@
+
+class Game
+
+	def lance_de
+		# à chaque tour ===> lancé de dé
+		entr=String.new
+
+		while entr != "\n"
+			puts "Tape entrée pour lancer le dé!!!!"
+			entr = gets
+		end
+
+		# initialiser le lancé de dé
+		@chifr = rand (1..6)
+		puts @chifr
+
+	end
+
+# puts lance_de
+
+	def verif_de
+		# - si 5 ou 6 : +1 (monter) et dire sur quelle marche est le joueur
+		if @chifr == 5 or @chifr == 6 # if @chifr.to_s.match(/[56]/)
+			mont
+		elsif @chifr == 1
+			desc
+		else 
+			rest
+
+
+		end
+			
+
+	end
+
+	def pos_jou
+		#compteur de marche
+		@pos_mar 
+	end
+
+# puts comp_ma
+
+
+	def mont
+	# - si 5 ou 6 : +1 (monter) et dire sur quelle marche est le joueur
+		
+			@pos_mar += 1
+			puts "Vous êtes à la marche #{@pos_mar}"
+		
+		return @pos_mar
+	
+	end
+
+# puts mont
+
+
+
+# def desc
+# 	# - si 1 : -1 (descendre) et dire sur quelle marche est le joueur
+# end
+
+# def rest
+# 	# - si 2, 3 ou 4 : 0 (reste) et dire sur quelle marche est le joueur
+# end
+
+
+
+
+
+# def gagne
+# 	# quand 10eme marche atteint : GAGNE !!!!!!!
+# end
+
+
+# def perform
+# 	# additionne les autres method
+# end
+
+
+end
+
+var = Game.new
+var.lance_de
+# var.comp_ma
+var.mont
